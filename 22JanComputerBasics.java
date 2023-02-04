cc1b0222de1a3a9577da0139c1653c677407d075
1943R <1943modi@gmail.com>
2023-02-04 22:54:40 +0530

    22 jan 2023 Assignment

---
+++ 22JanComputerBasics.java
@@ -0,0 +1,81 @@
+****************************************************************
+1-
+A computer is an electronic device that manipulates information, 
+or data. It has the ability to store, retrieve, and process data. 
+You may already know that we can use a computer to type documents, 
+send email, play games, and browse the Web. You can also use it to
+ edit or create spreadsheets, presentations, and even videos.
+
+*****************************************************************
+
+2-
+RAM (Random Access Memory) is the hardware in a computing device 
+where the operating system (OS), application programs and data in 
+current use are kept so they can be quickly reached by the device's 
+processor. RAM is the main memory in a computer. It is much faster 
+to read from and write to than other kinds of storage, such as 
+a hard disk drive (HDD), solid-state drive (SSD) or optical drive.
+
+******************************************************************
+
+3-
+All data stored on storage media – whether that’s hard disk drives 
+(HDDs), solid state drives (SSDs), external hard drives, USB flash 
+drives, SD cards etc – can be converted to a string of bits, 
+otherwise known as binary digits. These binary digits have a value 
+of 1 or 0, and the strings can make up photos, documents, audio and video.
+A byte is the most common unit of storage and is equal to 8 bits.
+
+***********************************************************************
+
+4-
+Keyboard
+
+**********************************************************************
+
+5-
+There are many outputs created by a computer system. These include printed 
+documents, on-screen data and sound. An output device allows data to be 
+transmitted by the computer in a human-friendly form, for example, sound 
+being played through a speaker.
+
+Monitor - The main output device of a computer. It forms images by converting 
+		electrical energy into light in the form of tiny dots on the screen 
+		called pixels. The sharpness of the image depends upon the number of pixels.
+
+Printer - Used to print information on paper. Commonly uses either lasers or 
+		inkjets to precisely place ink onto the page to recreate text 
+		and high-quality images.
+
+Speaker - Converts digital signals into audible sound waves.
+
+Projector - A device that is used to project video output from the computer onto a wall or screen.
+
+*************************************************************************************
+
+6- 
+Mouse,joystick
+
+************************************************************************************
+
+7-
+Machine language gives instructions as 0's and 1's and is the only language that the computer understands.
+
+***********************************************************************
+
+8-
+
+Input devices
+An input device allows data such as text, images, video or sound to be entered into a computer system.
+
+Keyboard - The most common input device used for entering text. Includes keys for word processing 
+		and data inputting, and for performing specific functions.
+Mouse - The most popular pointing device used to control the cursor on the screen and activated 
+		when moved across a flat surface. There are usually two buttons that activate different 
+		features on the screen.
+Microphone - Used to convert analogue sound waves into a digital form that can be saved and 
+		manipulated by the computer. A microphone can also be used to input voice commands but 
+		they do not work well in noisy environments.
+Scanner - Works in a similar way to a photocopier. Captures images from the source which are then 
+		converted into a digital form that can be stored.
+************************************************************************************************
